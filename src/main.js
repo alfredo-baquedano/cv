75783d0ea384956c4d2f99bbbc3d054771c4252b
Alfredo Baquedano <alfredo.baquedano.c@gmail.com>
2019-08-25 19:30:23 -0400

    delete unnesessary route library

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from './router';
 import store from './store';
 import './registerServiceWorker';
 import vuetify from './plugins/vuetify';
@@ -11,7 +10,6 @@ import '@mdi/font/css/materialdesignicons.css';
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
   store,
   vuetify,
   render: h => h(App),
