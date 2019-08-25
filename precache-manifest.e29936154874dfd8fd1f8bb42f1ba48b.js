e406e2efc7e3718b6ccb979619039806eab442a3
Alfredo Baquedano <alfredo.baquedano.c@gmail.com>
2019-08-25 14:16:34 -0400

    Updates

---
+++ precache-manifest.e29936154874dfd8fd1f8bb42f1ba48b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8d2951fd936719ebc2f",
+    "url": "/js/about.b9414675.js"
+  },
+  {
+    "revision": "b5eec12bcb804b6f40c6",
+    "url": "/css/app.1531d0cd.css"
+  },
+  {
+    "revision": "b5eec12bcb804b6f40c6",
+    "url": "/js/app.51d08618.js"
+  },
+  {
+    "revision": "aaa81751810b0710f326",
+    "url": "/js/chunk-vendors.a0fd4fc0.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "69fc26fda8165bfd1dad31d5d14d5387",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
