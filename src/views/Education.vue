519c0f7ff765cada98044f192060e4c5c5da5d71
Alfredo Baquedano <alfredo.baquedano.c@gmail.com>
2019-09-19 20:03:51 -0300

    improve skills layout

--- src/views/Education.vue
+++ src/views/Education.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="education" style="width: 100%">
     <h1 class="display-1">Skills & Education</h1>
-    <v-row class="pa-12" justify="space-around">
-      <v-col cols="5" class="ma-5">
+    <v-row no-gutters justify="space-around">
+      <v-col cols="5" class="ma-12">
       </v-col>
       <v-divider inset vertical class="ma-5"/>
-      <v-col cols="5" class="ma-5">
+      <v-col cols="5" class="ma-12">
         <v-row no-gutters justify="space-around">
-          <v-col cols="6" class="pa-3" :key="skill.name" v-for="skill in skills">
+          <v-col
+            cols="6"
+            class="pa-3"
+            :key="skill.name"
+            v-for="skill in skills"
+          >
             <h3 class="subtitle">{{skill.name}}</h3>
             <v-row class="pb-5" justify="start">
               <v-col
@@ -23,9 +28,7 @@
                   :value="knowlodge.value"
                   color="primary"
                 >
-                  <span class="caption">
-                    {{ knowlodge.name }}
-                  </span>
+                  <span class="caption skill-chart-text">{{ knowlodge.name }}</span>
                 </v-progress-circular>
               </v-col>
             </v-row>
@@ -44,3 +47,8 @@ export default {
   }),
 };
 </script>
+<style>
+.skill-chart-text {
+  white-space: pre-line;
+}
+</style>
