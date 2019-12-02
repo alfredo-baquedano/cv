d16f14cae4974bfd9658688489cccc91991dd85f
AlfredoBaquedano <alfredo.baquedano.c@gmail.com>
2019-12-01 22:10:31 -0300

    add support for mdi icons

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
 import es from 'vuetify/es5/locale/es';
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.use(Vuetify);
 
