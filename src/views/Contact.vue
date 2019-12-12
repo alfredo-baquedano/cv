ef370c593c88dbea0c620102003567886b580c46
AlfredoBaquedano <alfredo.baquedano.c@gmail.com>
2019-12-11 23:16:55 -0300

    added contact section

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,37 @@
 <template>
   <div class="contact" style="height: 600px">
-    <h1>Contact placeholder</h1>
+    <h1 class="display-1">Contact</h1>
+    <v-col cols="7">
+      <v-text-field
+        prepend-icon="mdi-account"
+        label="Name"
+        dense
+        solo
+      ></v-text-field>
+      <v-text-field
+        prepend-icon="mdi-at"
+        label="Email"
+        dense
+        solo
+      ></v-text-field>
+      <v-text-field
+        prepend-icon="mdi-domain"
+        label="Company"
+        dense
+        solo
+      ></v-text-field>
+      <v-text-field
+        prepend-icon="mdi-comment"
+        label="Subject"
+        dense
+        solo
+      ></v-text-field>
+      <v-textarea
+        prepend-icon="mdi-comment-text"
+        label="Message"
+        dense
+        solo
+      ></v-textarea>
+    </v-col>
   </div>
 </template>
