04abfc48a016a22152b049141391437c349babed
AlfredoBaquedano <alfredo.baquedano.c@gmail.com>
2019-12-15 17:38:55 -0300

    complete about component

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,95 @@
 <template>
-  <div class="about" style="height: 600px">
+  <div class="about">
     <v-row justify="center">
       <h1 class="display-1 mb-12">About</h1>
+    </v-row>
+    <v-row no-gutters class="px-12">
+      <v-col class="px-12">
+        <v-card>
+          <v-card-title>
+            Personal Profile
+          </v-card-title>
+          <v-card-text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eu mi dapibus, varius purus vitae, dapibus nunc. Curabitur pellentesque varius ante interdum pellentesque. Aenean pretium sollicitudin augue quis gravida. Nunc a varius dolor. Duis at enim ante. Mauris id facilisis nisi. Maecenas sed bibendum sapien. In imperdiet ipsum magna, nec tincidunt tellus pulvinar et. Fusce sit amet porta elit, eget commodo massa. Duis viverra non mi in blandit. Nulla mollis dignissim sem ut sodales. Morbi faucibus leo massa.
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col class="px-12">
+        <v-card>
+          <v-card-title>
+            Basic Information
+          </v-card-title>
+          <v-card-text>
+            <v-list dense color="transparent">
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon color="primary">mdi-account</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title>
+                    Name
+                  </v-list-item-title>
+                  <v-list-item-subtitle>
+                    Lorem ipsum
+                  </v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon color="primary">mdi-email</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title>
+                    Email
+                  </v-list-item-title>
+                  <v-list-item-subtitle>
+                    Lorem ipsum
+                  </v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon color="primary">mdi-phone</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title>
+                    Phone
+                  </v-list-item-title>
+                  <v-list-item-subtitle>
+                    Lorem ipsum
+                  </v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon color="primary">mdi-calendar</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title>
+                    Date of birth
+                  </v-list-item-title>
+                  <v-list-item-subtitle>
+                    Lorem ipsum
+                  </v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon color="primary">mdi-map-marker</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title>
+                    Address
+                  </v-list-item-title>
+                  <v-list-item-subtitle>
+                    Lorem ipsum
+                  </v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-card-text>
+        </v-card>
+      </v-col>
     </v-row>
   </div>
 </template>
