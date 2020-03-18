fb9dd314741e39325fd51249d9f1504f292a03ad
Alfredo <alfredo.baquedano.c@gmail.com>
2020-03-18 18:35:33 -0300

    add flat animations in education view

--- src/views/Education.vue
+++ src/views/Education.vue
@@ -3,59 +3,68 @@
     <v-row justify="center">
       <h1 class="display-1 mb-12">Skills & Education</h1>
     </v-row>
-    <v-row no-gutters justify="space-around" class="px-12">
-      <v-col class="pa-12">
-        <v-alert
-          border="left"
-          colored-border
-          color="primary"
-          elevation="2"
-          v-for="(education, index) in educations"
-          :key="index"
-          :icon="education.type"
-          prominent
-        >
-          <p class="subtitle-2">{{education.name}}</p>
-          <p class="caption">{{education.description}}</p>
-        </v-alert>
-      </v-col>
-      <v-divider inset vertical/>
-      <v-col class="pa-12">
-        <v-slide-x-transition
-          group
-          v-intersect="onSkillsIntersect"
-          no-gutters
-          justify="space-around"
-          tag="v-row">
-          <v-col
-            v-show="areSkillsIntersecting"
-            cols="6"
-            class="pa-3"
-            :key="skill.name"
-            v-for="skill in skills"
+    <v-row
+      no-gutters
+      justify="space-around"
+      class="px-12"
+      v-intersect="{ handler: onSkillsIntersect, options: { threshold: [0, 0.25, 0.5, 0.75, 1] }}">
+      <v-slide-x-transition>
+        <v-col
+          v-show="areSkillsIntersecting"
+          class="pa-12">
+          <v-alert
+            border="left"
+            colored-border
+            color="primary"
+            elevation="2"
+            v-for="(education, index) in educations"
+            :key="index"
+            :icon="education.type"
+            prominent
           >
-            <h3 class="subtitle">{{skill.name}}</h3>
-            <v-row class="pb-5" justify="start">
-              <v-col
-                cols="auto"
-                class="pa-5"
-                :key="knowlodge.name"
-                v-for="knowlodge in skill.knowledges"
-              >
-                <v-progress-circular
-                  :rotate="360"
-                  :size="80"
-                  :width="5"
-                  :value="knowlodge.value"
-                  color="primary"
+            <p class="subtitle-2">{{education.name}}</p>
+            <p class="caption">{{education.description}}</p>
+          </v-alert>
+        </v-col>
+      </v-slide-x-transition>
+      <v-divider inset vertical/>
+      <v-slide-x-reverse-transition>
+        <v-col
+          v-show="areSkillsIntersecting"
+          class="pa-12">
+          <v-row
+            no-gutters
+            justify="space-around">
+            <v-col
+              v-show="areSkillsIntersecting"
+              cols="6"
+              class="pa-3"
+              :key="skill.name"
+              v-for="skill in skills"
+            >
+              <h3 class="subtitle">{{skill.name}}</h3>
+              <v-row class="pb-5" justify="start">
+                <v-col
+                  cols="auto"
+                  class="pa-5"
+                  :key="knowlodge.name"
+                  v-for="knowlodge in skill.knowledges"
                 >
-                  <span class="caption skill-chart-text">{{ knowlodge.name }}</span>
-                </v-progress-circular>
-              </v-col>
-            </v-row>
-          </v-col>
-        </v-slide-x-transition>
-      </v-col>
+                  <v-progress-circular
+                    :rotate="360"
+                    :size="80"
+                    :width="5"
+                    :value="knowlodge.value"
+                    color="primary"
+                  >
+                    <span class="caption skill-chart-text">{{ knowlodge.name }}</span>
+                  </v-progress-circular>
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-slide-x-reverse-transition>
     </v-row>
   </div>
 </template>
@@ -71,8 +80,8 @@ export default {
     educations: state => state.person.education,
   }),
   methods: {
-    onSkillsIntersect (entries, observer) {
-      this.areSkillsIntersecting = entries[0].isIntersecting
+    onSkillsIntersect(entries) {
+      this.areSkillsIntersecting = entries[0].isIntersecting;
     },
   },
 };
