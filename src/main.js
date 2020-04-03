de0c30afd93a1895ce50f84b5aaa86fe3f5e492c
Alfredo <alfredo.baquedano.c@gmail.com>
2020-04-03 15:52:53 -0300

    update animation on scroll in education section

--- src/main.js
+++ src/main.js
@@ -6,10 +6,15 @@ import vuetify from './plugins/vuetify'
 import '@babel/polyfill'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 Vue.config.productionTip = false
 
 new Vue({
+    created () {
+        AOS.init()
+    },
     store,
     vuetify,
     render: h => h(App)
--- src/views/Education.vue
+++ src/views/Education.vue
@@ -1,65 +1,70 @@
 <template>
-  <div class="education" style="width: 100%">
-    <v-row justify="center">
-      <h1 class="display-1 mb-12">Skills & Education</h1>
-    </v-row>
-    <v-row
-      no-gutters
-      justify="space-around"
-      class="px-12">
-      <v-col
-        class="pa-12">
-        <v-alert
-          border="left"
-          colored-border
-          color="primary"
-          elevation="2"
-          v-for="(education, index) in educations"
-          :key="index"
-          :icon="education.type"
-          prominent
-        >
-          <p class="subtitle-2">{{education.name}}</p>
-          <p class="caption">{{education.description}}</p>
-        </v-alert>
-      </v-col>
-      <v-divider inset vertical/>
-      <v-col
-        class="pa-12">
-        <v-row
-          no-gutters
-          justify="space-around">
-          <v-col
-            v-show="areSkillsIntersecting"
-            cols="6"
-            class="pa-3"
-            :key="skill.name"
-            v-for="skill in skills"
-          >
-            <h3 class="subtitle">{{skill.name}}</h3>
-            <v-row class="pb-5" justify="start">
-              <v-col
-                cols="auto"
-                class="pa-5"
-                :key="knowlodge.name"
-                v-for="knowlodge in skill.knowledges"
-              >
-                <v-progress-circular
-                  :rotate="360"
-                  :size="80"
-                  :width="5"
-                  :value="knowlodge.value"
-                  color="primary"
-                >
-                  <span class="caption skill-chart-text">{{ knowlodge.name }}</span>
-                </v-progress-circular>
-              </v-col>
-            </v-row>
-          </v-col>
+    <div class="education" style="width: 100%">
+        <v-row justify="center">
+        <h1 class="display-1 mb-12">Skills & Education</h1>
         </v-row>
-      </v-col>
-    </v-row>
-  </div>
+        <v-row
+            no-gutters
+            justify="space-around"
+            class="px-12">
+            <v-col
+                class="skills-item pa-12">
+                <v-alert
+                    border="left"
+                    colored-border
+                    color="primary"
+                    elevation="2"
+                    v-for="(education, index) in educations"
+                    :key="index"
+                    :icon="education.type"
+                    data-aos="fade-right"
+                    :data-aos-delay="index*100"
+                    data-aos-anchor=".skills-item"
+                    prominent>
+                <p class="subtitle-2">{{education.name}}</p>
+                <p class="caption">{{education.description}}</p>
+                </v-alert>
+            </v-col>
+            <v-divider inset vertical/>
+            <v-col
+                class="pa-12">
+                <v-row
+                class="education-item"
+                no-gutters
+                justify="space-around">
+                <v-col
+                    cols="6"
+                    class="pa-3"
+                    :key="skill.name"
+                    v-for="(skill, index) in skills"
+                    data-aos="fade-left"
+                    :data-aos-delay="index*100"
+                    data-aos-anchor=".education-item"
+                >
+                    <h3 class="subtitle">{{skill.name}}</h3>
+                    <v-row class="pb-5" justify="start">
+                    <v-col
+                        cols="auto"
+                        class="pa-5"
+                        :key="knowlodge.name"
+                        v-for="knowlodge in skill.knowledges"
+                    >
+                        <v-progress-circular
+                        :rotate="360"
+                        :size="80"
+                        :width="5"
+                        :value="knowlodge.value"
+                        color="primary"
+                        >
+                        <span class="caption skill-chart-text">{{ knowlodge.name }}</span>
+                        </v-progress-circular>
+                    </v-col>
+                    </v-row>
+                </v-col>
+                </v-row>
+            </v-col>
+        </v-row>
+    </div>
 </template>
 <script>
 import { mapState } from 'vuex'
