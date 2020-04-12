67202ebbc98180d77ee947e57dba41eaf59f13df
Alfredo <alfredo.baquedano.c@gmail.com>
2020-04-12 02:13:22 -0400

    improve experience section

--- src/views/Experience.vue
+++ src/views/Experience.vue
@@ -1,45 +1,46 @@
 <template>
-  <div>
-    <v-row justify="center">
-      <h1 class="display-1 mb-12">Experience</h1>
-    </v-row>
-    <v-row justify="center">
-      <v-col cols="9" class="pa-5">
-        <v-timeline>
-          <v-timeline-item
-            v-for="(experience, index) in experiences"
-            :key="index"
-            :data-aos="(index % 2 === 0) ? 'fade-left' : 'fade-right'"
-            :data-aos-delay="index*150"
-            data-aos-anchor="#experience-section"
-            :color="experience.company.color"
-            small
-          >
-            <template v-slot:icon>
-              <v-avatar>
-                <img :src="experience.company.logo">
-              </v-avatar>
-            </template>
-            <template v-slot:opposite>
-              <span
-                :class="`headline font-weight-bold primary--text`">
-                {{experience.year}}
-              </span>
-            </template>
-            <v-card class="py-4">
-              <v-card-title
-                :class="`headline font-weight-light mb-4 primary--text`">
-                {{experience.position}}
-              </v-card-title>
-              <v-card-text>
-                {{experience.description}}
-              </v-card-text>
-            </v-card>
-          </v-timeline-item>
-        </v-timeline>
-      </v-col>
-    </v-row>
-  </div>
+    <div>
+        <v-row justify="center">
+            <h1 class="display-1 mb-12">Experience</h1>
+        </v-row>
+        <v-row justify="center">
+            <v-col cols="9" class="pa-5">
+            <v-timeline>
+                <v-timeline-item
+                v-for="(experience, index) in experiences"
+                :key="index"
+                :data-aos="(index % 2 === 0) ? 'fade-left' : 'fade-right'"
+                :data-aos-delay="index*150"
+                data-aos-anchor="#experience-section"
+                :color="experience.company.color"
+                small>
+                <template v-slot:icon>
+                    <v-avatar>
+                        <img :src="experience.company.logo">
+                    </v-avatar>
+                </template>
+                <template v-slot:opposite>
+                    <span class="headline font-weight-bold primary--text">
+                    {{ experience.year }}
+                    </span>
+                </template>
+                <v-card class="py-4">
+                    <v-card-title class="headline font-weight-light primary--text mb-0">
+                        {{ experience.position }}
+                    </v-card-title>
+                    <v-card-text>
+                        <ul class="pl-4" style="list-style-type: disc">
+                            <li v-for="(duty, index) in experience.duties" :key="index">
+                                {{ duty }}
+                            </li>
+                        </ul>
+                    </v-card-text>
+                </v-card>
+                </v-timeline-item>
+            </v-timeline>
+            </v-col>
+        </v-row>
+    </div>
 </template>
 <script>
 import { mapState } from 'vuex'
